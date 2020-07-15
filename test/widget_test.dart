276e0560782b9c59e02371a20099ecd8a53a712a
pch720 <52265916+pch720@users.noreply.github.com>
2020-07-16 02:28:32 +0900

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:netflix_clone_test/main.dart';
 
 void main() {
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
